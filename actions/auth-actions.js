e8ff07c4f3e9dbe8b4f6988678d9cefd195df552
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-03 23:59:06 +0300

    write action signup

---
+++ actions/auth-actions.js
@@ -0,0 +1,18 @@
+'use server';
+
+export async function signup(prevState, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  let errors = {};
+
+  if (!email.includes('@')) {
+    errors.email = 'Please enter a valid email address';
+  }
+
+  if (password.trim().length < 0) {
+    errors.password = 'Password must be at least 8 characters';
+  }
+
+  if()
+}
