cd1ad483bf3351adba43cc79eb9bfb08e376cba7
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-08-04 23:35:30 +0300

    write logim functions

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,8 +1,8 @@
 'use server';
 import { redirect } from 'next/navigation';
 
-import { hashUserPassword } from '@/lib/hash';
-import { createUser } from '@/lib/users';
+import { hashUserPassword, verifyPassword } from '@/lib/hash';
+import { createUser, getUserByEmail } from '@/lib/users';
 import { createAuthSession } from '@/lib/auth';
 
 export async function signup(prevState, formData) {
@@ -39,4 +39,40 @@ export async function signup(prevState, formData) {
 
     throw error;
   }
+}
+
+export async function login(prevState, formData) {
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: 'Could not authenticate user, please check your credentials.',
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: 'Could not authenticate user, please check your credentials.',
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect('/training');
+}
+
+export async function auth(mode, prevState, formData) {
+  if (mode === 'login') {
+    return login(prevState, formData);
+  }
+
+  return signup(prevState, formData);
 }
